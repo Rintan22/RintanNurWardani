a70950c221cfb74e067efbd61049f44b0b7d5485
Rintan22 <nurwardanirintan@gmail.com>
2026-02-07 10:18:37 +0700

    Menambahkan nama, NIM, dan kelas pada title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+       <title>Laravel - Rintan Nur Wardani | NIM: E41242246 | Kelas: F</title>
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
